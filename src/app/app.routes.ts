5e270efadd9565bf849fb39ad29f87d2fc191a27
Fransuelton <elton6103@gmail.com>
2025-06-13 06:21:17 -0300

    ✨ feat: define application routes for Home, Registration, and Listing components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
+import { Home } from './components/home/home';
+import { Registration } from './components/registration/registration';
+import { Listing } from './components/listing/listing';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: Home,
+    title: 'Início – Cadastro e Consulta de Processos | DataJud CNJ',
+  },
+  {
+    path: 'partes-interessadas',
+    component: Registration,
+    title: 'Cadastro de Partes Interessadas | DataJud CNJ',
+  },
+  {
+    path: 'consulta-processos',
+    component: Listing,
+    title: 'Consulta Pública de Processos – TRF1 | DataJud CNJ',
+  },
+];
