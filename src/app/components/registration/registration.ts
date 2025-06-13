f260e4582e21abe5cb3f9a5acbe04b626ad7735c
Fransuelton <elton6103@gmail.com>
2025-06-12 22:35:40 -0300

    ✨ feat: add registration component with HTML template and unit tests

---
+++ src/app/components/registration/registration.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import {FormControl, ReactiveFormsModule} from '@angular/forms'
+
+@Component({
+  selector: 'app-registration',
+  imports: [ReactiveFormsModule],
+  templateUrl: './registration.html',
+})
+export class Registration {
+
+}
