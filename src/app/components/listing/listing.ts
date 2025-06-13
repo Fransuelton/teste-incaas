6769fdf249f1563823f60bc7fbd890d11eaa465f
Fransuelton <elton6103@gmail.com>
2025-06-13 07:52:06 -0300

    ✨ feat: enhance listing component with filtering, sorting, and modal functionality

--- src/app/components/listing/listing.ts
+++ src/app/components/listing/listing.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 import { DataJud } from '../../services/data-jud';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { ListingModal } from '../listing-modal/listing-modal';
 
 @Component({
   selector: 'app-listing',
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule, ListingModal],
   templateUrl: './listing.html',
 })
 export class Listing implements OnInit {
   data: any[] = [];
   error: boolean = false;
+
+  selectedGrau: string = '';
+  selectedJustica: string = '';
+  sortDesc: boolean = false;
+
+  filteredData: any[] = [];
+
+  selectedProcess: any = null;
+  showModal: boolean = false;
 
   constructor(private DataJud: DataJud) {}
 
@@ -17,7 +28,7 @@ export class Listing implements OnInit {
     this.DataJud.getData().subscribe({
       next: (data) => {
         this.data = data.hits.hits.map((item: any) => item._source);
-
+        this.applyFilters();
         console.log(this.data);
       },
       error: (error) => {
@@ -28,5 +39,45 @@ export class Listing implements OnInit {
         console.log('Requisição finalizada');
       },
     });
+  }
+
+  applyFilters() {
+    this.filteredData = this.data.filter((item) => {
+      const justica = item.tribunal.startsWith('TRF') ? 'Federal' : 'Estadual';
+
+      const grauMatch = this.selectedGrau
+        ? item.grau === this.selectedGrau
+        : true;
+      const justicaMatch = this.selectedJustica
+        ? justica === this.selectedJustica
+        : true;
+
+      return grauMatch && justicaMatch;
+    });
+
+    this.sortByAssuntos();
+  }
+
+  sortByAssuntos() {
+    this.filteredData.sort((a, b) => {
+      const aCount = a.assuntos?.length || 0;
+      const bCount = b.assuntos?.length || 0;
+      return this.sortDesc ? bCount - aCount : aCount - bCount;
+    });
+  }
+
+  toggleSort() {
+    this.sortDesc = !this.sortDesc;
+    this.sortByAssuntos();
+  }
+
+  openModal(process: any) {
+    this.selectedProcess = process;
+    this.showModal = true;
+  }
+
+  closeModal() {
+    this.showModal = false;
+    this.selectedProcess = null;
   }
 }
